17b2d97ae8b0096dcfa6829d89af0aa5f89737ed
Peter van der Zee <github-public@qfox.nl>
2016-10-05 21:53:48 +0200

    [BROKEN FORCED] wip

--- src/doms/domain_plus.js
+++ src/doms/domain_plus.js
@@ -27,6 +27,7 @@ import {
   STR_RANGE_SIZE,
   STR_VALUE_SIZE,
 
+  domain__debug,
   domain_str_closeGaps,
   domain_num_createRange,
   domain_str_decodeValue,
@@ -97,7 +98,9 @@ function _domain_plusWillBeSmall(domain1, domain2) {
   ASSERT(typeof domain1 === 'number', 'ONLY_WITH_NUMBERS');
   ASSERT(typeof domain2 === 'number', 'ONLY_WITH_NUMBERS');
   // if both domains are small enough they cannot add to a domain beyond the max
-  if (domain1 < NINE && domain2 < EIGHT) return true; // this shortcut catches most cases
+
+  //if (((domain1 | domain2) >>> 0) < (1 << 15)) return true; // this shortcut catches most cases
+  if (domain1 < (1<<15) && domain2 < (1<<15)) return true; // this shortcut catches most cases
   return domain_max(domain1) + domain_max(domain2) <= SMALL_MAX_NUM; // if max changes, update above too!
 }
 function _domain_plusNumNumStr(domain1, domain2) {
